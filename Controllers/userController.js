2874268c9ae8abaeec71e666cf3722b4cbb79b29
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-05-16 22:33:40 +0000

    login done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -46,7 +46,46 @@ const signup = async (req, res) => {
     
 }
 
+//login
+
+const login = async (req, res) => {
+
+    try {
+        const { email, password } = req.body
+
+        const user = await User.findOne({ email })
+
+        //if user email is found, compare password
+        if(user){
+            const isSame = await bcrypt.compare(password, user.password)
+
+            if(isSame){
+                let token = jwt.sign({ id : user.id },userConfig.secret, {
+                    expiresIn: 1 * 24 * 60 * 60 * 1000
+                })
+                // const token = generateToken(user.id)
+                //if password matches wit the one in the database
+        //go ahead and generate a cookie for the user
+                res.cookie('jwt', token, { maxAge: 1 * 24 * 60 * 60 , httpOnly: true})
+                    console.log('user', JSON.stringify(user, null, 2))
+                    console.log(token)
+             return res.status(201).send(user)  
+            }else{
+                return res.status(401).send('Authentication failed')
+            }
+        }else{
+            return res.status(401).send('Authentication failed')
+        }
+        
+       
+        
+    } catch (error) {
+      console.log(error)  
+    }
+}
+
 
 module.exports = {
-    signup
+    signup,
+    login
 }
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const userController = require('../Controllers/userController')
-const { signup } = userController
+const { signup, login } = userController
 const userAuth = require('../Middlewares/userAuth')
 
 const router = express.Router()
 
 //signup endpoint
 router.post('/signup', userAuth.saveUser, signup)
+
+//login route
+router.post('/login', login )
 
 module.exports = router
